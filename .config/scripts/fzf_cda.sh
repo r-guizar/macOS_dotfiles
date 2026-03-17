507b2fdc5b427c27cfcdba5d8e2b5e0a6f85f273
Richard Guizar <60202563+r-guizar@users.noreply.github.com>
2026-03-17 09:32:45 -0400

    Updated fzf scripts

--- .config/scripts/fzf_cda.sh
+++ .config/scripts/fzf_cda.sh
@@ -9,12 +9,6 @@ fi
 # fd - cd to selected directory
 base_dir="${1:-./}"
 
-if [[ $base_dir == $HOME ]]; then
-    sed_cmd='sed "s|^$HOME|~|"'
-else
-    sed_cmd='sed "s|^$base_dir||"'
-fi
-
 dir=$(fd --hidden --max-depth 10 --type d . "$base_dir" \
   | sed "s|^$HOME|~|" \
   | fzf --tmux +m)
